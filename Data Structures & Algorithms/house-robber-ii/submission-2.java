5c3f95612a05b52b9fbcbed555b2aa5b3c317f1f
Sanjay Kumar <50935808+sanjay-dynasty@users.noreply.github.com>
2026-08-07 07:14:51 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int solve(int nums[], int l, int r) {
+        int include = nums[l];
+        int exclude = 0;
+
+        for (int i = l + 1; i <= r; i++) {
+            int curr = Math.max(exclude + nums[i], include);
+            exclude = include;
+            include = curr;
+        }
+        return include;
+    }
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        int rob1 = solve(nums, 0, nums.length - 2);
+        int rob2 = solve(nums, 1, nums.length - 1);
+
+        return Math.max(rob1, rob2);
+    }
+}
